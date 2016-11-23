5e1bd5d0d718554096ede4f05b8efb3e7b8e6dae
Matthew Marcus <osirun@gmail.com>
2016-11-23 11:38:55 +0000

    Second test

--- test/primestable_test.exs
+++ test/primestable_test.exs
@@ -6,5 +6,9 @@ defmodule PrimestableTest do
 
   test "gets the first prime" do
     assert get_primes(1) == [2]
+  end
+
+  test "gets the first two primes" do
+    assert get_primes(2) == [2, 3]
   end  
 end
