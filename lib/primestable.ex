e6e83159f00137cedd08b6f6260e4a55638a7dc8
Matthew Marcus <osirun@gmail.com>
2016-11-23 12:57:59 +0000

    Ultra-naive implementation for small ranges of primes

--- lib/primestable.ex
+++ lib/primestable.ex
@@ -1,7 +1,17 @@
 defmodule Primestable do
 
   def get_primes(n) do
-    Enum.map 1..n, fn(x) -> x + 1 end
-  end  
+    Enum.take primes, n
+  end
+
+  defp primes do 
+    Enum.filter 2..10, &is_prime/1
+  end
+
+  defp is_prime(2), do: true
+  defp is_prime(n) do 
+    # a prime is not divisible by any number between 1 and itself
+    Enum.all? 2..n-1, fn(x) -> rem(n, x) != 0 end
+  end 
 
 end
