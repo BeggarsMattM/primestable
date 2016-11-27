72233c17d2be98f9d1222e17ce82e84a1ac02247
Matthew Marcus <osirun@gmail.com>
2016-11-27 08:33:57 +0000

    Melissa O'Neill inspired lazy primes generator. Currently generates only ascending values

--- lib/primes.ex
+++ lib/primes.ex
@@ -3,8 +3,8 @@ defmodule Primes do
 
   def get_first_n_primes(n) when is_integer(n) and n > 0 do
     profile do
-      Enum.take primes, n
-      IO.puts "message\n"
+      primes = Enum.take oneill_sieve, n
+      IO.inspect primes
     end  
   end
   def get_first_n_primes(_), do: []
@@ -29,4 +29,37 @@ defmodule Primes do
       _  -> sieved(filtered_candidates, [ (hd(filtered_candidates)) | primes ])
     end    
   end
+
+  def different_sieve do
+    Stream.unfold(2..100_000, &next_prime/1)
+  end
+  def next_prime(collection) do
+    first = hd(Enum.take collection, 1)
+    new_collection = Stream.filter(collection, fn(x) -> rem(x, first) != 0 end)
+    {first, new_collection}
+  end
+
+  def oneill_sieve do
+    Stream.unfold(prelim_state, fn(state) -> {state.current, next(state)} end)
+  end  
+
+  def prelim_state do
+    %{current: 2, lookups: HashDict.new}
+  end
+
+  def next(%{current: current, lookups: lookups}) do
+    case lookups[current] do 
+      nil -> %{ current: current + 1, lookups: add_to(lookups, current) }
+      stream -> next(%{current: current + 1, lookups: amend_for(lookups, current)}) 
+    end
+  end
+
+  def add_to(lookups, current) do
+    lookups
+  end  
+
+  def amend_for(lookups, current) do
+    lookups
+  end
+
 end
